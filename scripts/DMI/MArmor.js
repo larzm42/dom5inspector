be3ff4f1e235681611cc73faeb953e0ab4d9b12c
logg-y <loggyc4@gmail.com>
2021-07-26 16:36:23 +0100

    initialise armour def bonus to 0 not 10, handle prot on a type-by-type basis for modded items

--- scripts/DMI/MArmor.js
+++ scripts/DMI/MArmor.js
@@ -15,7 +15,7 @@ var modconstants = DMI.modconstants;
 
 MArmor.initArmor = function(o) {
 	o.type = 5;
-	o.def = 10;
+	o.def = 0;
 	o.used_by = [];
 }
 
@@ -66,7 +66,15 @@ MArmor.prepareData_PostMod = function() {
 		}
 
 		if (o.prot) {
-			o.protbody = o.prot;
+			if (o.type == 4) {
+				o.protshield = o.prot;
+			} else if (o.type == 6) {
+				o.prothead = o.prot;
+			} else if (o.type == 8) {
+				o.general = o.prot;
+			} else {
+				o.protbody = o.prot;
+			}
 		}
 
 		if (o.general) {
